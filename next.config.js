df21b507f8971d581ccdff66437e89375f49e8ea
Aviv Eldan <39082532+aviveldan@users.noreply.github.com>
2025-07-25 12:34:58 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Images configuration can be extended to allow remote domains if needed.
+  images: {
+    domains: [],
+  },
+};
+
+module.exports = nextConfig;
